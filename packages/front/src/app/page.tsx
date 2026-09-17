8cff8e0847bdae2951a69a02fb0bba6bd3338654
souza-luiza <souza.luiza@unifesp.br>
2026-09-16 22:36:27 -0300

    feat: renderiza os componentes routes de cada módulo na página inicial

--- packages/front/src/app/page.tsx
+++ packages/front/src/app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx
 
 import { HomeSearch } from '@/components/features/home-search/home-search'
+import { Route } from '@/components/features/route/route'
 import { Hero } from '@/components/site-blocks/hero'
 
 export default function Home() {
@@ -8,6 +9,13 @@ export default function Home() {
     <div className="flex justify-center min-h-screen w-full bg-site-background">
       <main className="max-w-5xl flex-col py-20 px-6 sm:px-16">
         <Hero />
+        <ul className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-2.5 max-w-[820px] mx-auto mt-10">
+          <Route module={'1'} />
+          <Route module={'2'} />
+          <Route module={'3'} />
+          <Route module={'4'} />
+          <Route module={'5'} />
+        </ul>
         <div className="mt-10">
           <HomeSearch />
         </div>
