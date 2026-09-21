0e6c875af5eacd8f930b320e9c59acbd8e0d9c84
ric <rirenoart@gmail.com>
2026-09-21 07:59:13 -0300

    Resolve conflitos de merging (lint do github está causando divergências)

--- packages/bulker/internal/elastic/mapping.go
+++ packages/bulker/internal/elastic/mapping.go
@@ -3,6 +3,9 @@ package elastic
 import _ "embed"
 
 // CursosMapping é o mapping do índice principal, embutido no binário.
+<<<<<<< HEAD
 //
+=======
+>>>>>>> 89d2749 (Retorna o container bulker e adiciona setup.sh)
 //go:embed mapping.json
 var CursosMapping []byte
